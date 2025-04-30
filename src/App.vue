abdd960c99a0e649ecc6ebb32ef711ffd3188959
bachardaowd <bachar.daowd@gmail.com>
2025-04-30 13:40:13 +0200

    hello world

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>{{ message }}</div>
+</template>
+
+<script>
+export default {
+  setup() {
+    const message="Hello World";
+    return {message};
+  }
+}
+</script>
+<style>
+</style>
